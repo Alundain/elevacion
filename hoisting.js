aae5c92e5800c09963bdaabcaec5b2937a3f0444
Dorian Donoso Ampuero <adonosoaster@gmail.com>
2021-04-10 20:27:33 -0400

    first commit

---
+++ hoisting.js
@@ -0,0 +1,82 @@
+//1
+//console.log(hello);                                   
+//var hello = 'world'; 
+
+//console.log(hello);    UNDEFINED                               
+//var hello = 'world'; HELLO=WORLD
+
+//2
+//var needle = 'haystack';
+//test();
+//function test(){
+   // var needle = 'magnet';
+   // console.log(needle);
+//}
+
+//var needle = 'haystack';
+//test();
+//function test(){
+    //var needle = 'magnet';
+    //console.log(needle);
+//}
+//console.log(needle);
+//test hacer log
+//needle ='haystack';
+//log 'magnet'
+
+//3
+
+/*var brendan = 'super cool';
+function print(){
+    brendan = 'only okay';
+    console.log(brendan);
+}
+console.log(brendan);*/
+//print log brendan
+//Brendan='super cool'
+//log 'super cool'
+
+//let brendan = 'super cool';
+//const print = () => {
+    //brendan = 'only okay';
+    //console.log(brendan);
+//}
+//console.log(brendan);
+
+//7
+/*
+dojo = "san jose";
+console.log(dojo);
+learn();
+function learn() {
+    dojo = "seattle";
+    console.log(dojo);
+    var dojo = "burbank";
+    console.log(dojo);
+}
+console.log(dojo);
+*/
+
+//learn hace log
+//dojo = 'san jose'
+//log 'san jose'
+//nuevo dojo 'seattle'
+//nuevo dojo 'burbank'
+//log 'burbank'
+/*
+const dojo = "san jose";
+console.log(dojo);
+const learn = () => {
+    let dojo = "seattle";
+    console.log(dojo);
+    var dojo = "burbank";
+    console.log(dojo);
+}
+learn();
+console.log(dojo);
+*/
+
+
+
+
+
