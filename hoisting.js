60e4244d972e5cd48913d1114d544f843b5c80dc
Dorian Donoso <adonosoaster@gmail.com>
2021-06-06 13:49:32 -0400

    Correccion de errores

--- hoisting.js
+++ hoisting.js
@@ -1,16 +1,16 @@
 //1
 //console.log(hello);                                   
 //var hello = 'world'; 
+//console.log(hello);    //UNDEFINED                               
+//var hello = 'world'; //HELLO=WORLD
 
-//console.log(hello);    UNDEFINED                               
-//var hello = 'world'; HELLO=WORLD
 
 //2
 //var needle = 'haystack';
 //test();
 //function test(){
-   // var needle = 'magnet';
-   // console.log(needle);
+//var needle = 'magnet';
+//console.log(needle);
 //}
 
 //var needle = 'haystack';
